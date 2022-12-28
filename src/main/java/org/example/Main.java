ddc1a34645b887799293b2736ed5c3362fca4bab
vsravanireddy <115485911+vsravanireddy@users.noreply.github.com>
2022-12-28 19:10:14 +0530

    Update Main.java
    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Sravani Vulupala!");
-        System.out.println("This is a line of code added to commit and trigger a new build in jenkins");
+        System.out.println("This is a line of code added to commit and trigger a new build in jenkins.");
     }
 }
