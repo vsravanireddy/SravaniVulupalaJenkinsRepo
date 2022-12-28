2025d76894e7d773f97b254301c7cec655364f8b
svulupal <svulupal@opentext.com>
2022-12-28 13:01:55 +0530

    Added code to trigger a new build in jenkins

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello Sravani Vulupala!");
+        System.out.println("This is a line of code added to commit and trigger a new build in jenkins");
     }
 }
